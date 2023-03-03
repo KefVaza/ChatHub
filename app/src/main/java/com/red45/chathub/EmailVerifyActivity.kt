e129d0d484e393b33a619c6111a270273ac12e83
kefVaza <kaifvaza4@gmail.com>
2023-03-03 21:38:36 +0530

    Email veryfication design pending (kaif vaza)

---
+++ app/src/main/java/com/red45/chathub/EmailVerifyActivity.kt
@@ -0,0 +1,11 @@
+package com.red45.chathub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailVerifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_veify)
+    }
+}
