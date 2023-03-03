7dcd06cc5d89c1a4549212fb6e37af37567ac658
Kiaf <kaifvaza4@gmail.com>
2023-03-03 10:42:34 +0530

    First activity completed by kaif vaza

--- app/src/main/java/com/red45/chathub/First.kt
+++ app/src/main/java/com/red45/chathub/First.kt
@@ -1,11 +1,24 @@
 package com.red45.chathub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.red45.chathub.databinding.ActivityFirstBinding
 
 class First : AppCompatActivity() {
+
+    lateinit var binding: ActivityFirstBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+        binding.registerBtn.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
