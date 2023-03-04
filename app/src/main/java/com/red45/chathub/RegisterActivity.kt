09822b34f7fee392e4a67f3d4cb83b2d9f0f548b
kefVaza <kaifvaza4@gmail.com>
2023-03-04 11:46:26 +0530

    Profile Image picker design and code completed by kaif vaza

--- app/src/main/java/com/red45/chathub/RegisterActivity.kt
+++ app/src/main/java/com/red45/chathub/RegisterActivity.kt
@@ -1,11 +1,40 @@
 package com.red45.chathub
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
+import com.red45.chathub.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 1
+    private var mImgUri: Uri? = null
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectImgBtn.setOnClickListener{
+            selectImage()
+        }
+    }
+    private fun selectImage(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data!= null && data.data != null){
+            mImgUri = data.data
+            Glide.with(this).load(mImgUri).into(binding.ciPickProImg)
+        }
     }
 }
